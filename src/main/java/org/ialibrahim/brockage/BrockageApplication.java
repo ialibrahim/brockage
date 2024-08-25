fbb82c5d9a587a4a95584d68d0417af125946d5a
Ibraheem Alibraheem <ibraheem@poweronesoft.com>
2024-08-25 14:49:46 +0300

    Initial commit

---
+++ src/main/java/org/ialibrahim/brockage/BrockageApplication.java
@@ -0,0 +1,13 @@
+package org.ialibrahim.brockage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrockageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrockageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ialibrahim/brockage/BrockageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ialibrahim.brockage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrockageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
